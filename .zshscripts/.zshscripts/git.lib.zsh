89363757463f80595cdc8f95de47f56190bd3451
André Sousa <andrematosdesousa@gmail.com>
2025-05-04 15:29:32 +0100

    update zshrc raw

---
+++ .zshscripts/.zshscripts/git.lib.zsh
@@ -0,0 +1,11 @@
+# Custom script (loaded before prompt)
+function git_branch_name()
+{
+  branch=$(git symbolic-ref HEAD 2> /dev/null | awk 'BEGIN{FS="/"} {print $NF}')
+  if [[ $branch == "" ]];
+  then
+    :
+  else
+    echo ':('$branch')'
+  fi
+}
